e8175ac128f21000cc846c03676fbeaf30e3c111
Jiale Shang <shang8024@gmail.com>
2025-05-16 23:56:36 -0400

    Update page.tsx

--- next-app/app/(app)/[houseId]/page.tsx
+++ next-app/app/(app)/[houseId]/page.tsx
@@ -23,7 +23,7 @@ export default async function HousePage({ params }: { params: Promise<{ houseId:
     if (post.disabled === true) {
       notFound();
     }
-  } catch (_) {
+  } catch {
     notFound();
   }
   
